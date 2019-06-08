f548669cb751fe5f9379a05de13df8a4bcb5f8f8
Kennedy <jkennedyr3@gmail.com>
2019-06-08 17:07:35 -0300

    Função Edição

--- customers/functions.php
+++ customers/functions.php
@@ -14,3 +14,33 @@ function index() {
 	$customers = find_all('customers');
 }
 ?>
+
+<?php
+
+/**
+ *	Atualizacao/Edicao de Cliente
+ */
+function edit() {
+
+  $now = date_create('now', new DateTimeZone('America/Sao_Paulo'));
+
+  if (isset($_GET['id'])) {
+
+    $id = $_GET['id'];
+
+    if (isset($_POST['customer'])) {
+
+      $customer = $_POST['customer'];
+      $customer['modified'] = $now->format("Y-m-d H:i:s");
+
+      update('customers', $id, $customer);
+      header('location: index.php');
+    } else {
+
+      global $customer;
+      $customer = find('customers', $id);
+    } 
+  } else {
+    header('location: index.php');
+  }
+}
